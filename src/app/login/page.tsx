6b6cf4ec48323fc70ef376d63a976dc05ebd09f7
matheus <matheus.felipe.19rt@gmail.com>
2024-07-24 22:00:35 -0300

    style: Update Login page styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import { signIn, useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
 
-export default function LoginPage() {
+export default function Login() {
 	const { status } = useSession()
 
 	if (status === "authenticated") {
@@ -11,18 +11,19 @@ export default function LoginPage() {
 	}
 
 	return (
-		<div className="flex flex-col items-center justify-center">
+		<div className="flex flex-col items-center p-4">
 			<div className="w-full max-w-md rounded p-4 shadow-md">
-				<div className="flex flex-col items-center justify-center px-8 py-10 text-center">
-					<strong className="text-5xl">Sign In</strong>
+				<div className="flex flex-col items-center justify-center px-8 text-center">
+					<strong className="p-2 text-5xl">Sign In</strong>
 					<p className="mt-4 text-lg text-muted-foreground">Sign in with your preferred provider</p>
+					<hr className="my-4 w-full border-muted" />
 				</div>
 
-				<div className="flex flex-col items-center justify-center gap-4 p-4">
-					<button className="button w-72 bg-github text-accent-foreground" onClick={() => signIn("github")}>
+				<div className="flex flex-col items-center justify-center gap-4 p-6">
+					<button className="button w-full bg-github text-accent-foreground" onClick={() => signIn("github")}>
 						Sign In With GitHub
 					</button>
-					<button className="button w-72 bg-google text-accent-foreground" onClick={() => signIn("google")}>
+					<button className="button w-full bg-google text-accent-foreground" onClick={() => signIn("google")}>
 						Sign In With Google
 					</button>
 				</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 	})
 
 	return (
-		<div className="flex flex-col items-center justify-center gap-4">
+		<div className="flex flex-col items-center p-4">
 			<button className="button" onClick={() => refetch()}>
 				Hello World!
 			</button>
