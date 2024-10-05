849f74f735a3af9a5de943e9152b562a41774d2d
matheus <matheus.felipe.19rt@gmail.com>
2024-10-05 14:32:11 -0300

    chore: Update Playwright config & gitignore

--- playwright.config.js
+++ playwright.config.js
@@ -3,16 +3,19 @@ import { defineConfig } from "@playwright/test"
 export default defineConfig({
 	testDir: "./tests",
 	outputDir: "./tests/test-results",
+	reporter: [["list"], ["json", { outputFile: "./tests/test-results/report.json" }]],
 	use: {
 		headless: true,
-		screenshot: "only-on-failure",
-		video: "retain-on-failure",
+		coverage: {
+			enabled: true,
+			dir: "coverage",
+		},
 	},
 	webServer: {
-		reuseExistingServer: !process.env.CI,
 		command: "npm run dev",
 		port: 3000,
 		stdout: "ignore",
 		stderr: "pipe",
+		reuseExistingServer: !process.env.CI,
 	},
 })
